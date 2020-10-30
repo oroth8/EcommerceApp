a554fb13baec8b66766197e918535669556befd1
k1te <kmiller343@gmail.com>
2020-10-30 17:36:27 -0500

    wrote basic product model

---
+++ models/product.js
@@ -0,0 +1,10 @@
+module.exports = function(sequelize, DataTypes) {
+    let Product = sequelize.define("Product", {
+        brand: DataTypes.STRING,
+        name: DataTypes.STRING,
+        category: DataTypes.STRING,
+        subCategory: DataTypes.STRING,
+        price: DataTypes.DECIMAL(10,2),
+        image_URLs: DataTypes.STRING
+    })
+}
