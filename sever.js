cc0eedb939dbf476744f670df8b21b8f4178d71c
k1te <kmiller343@gmail.com>
2020-10-30 17:35:48 -0500

    required models/added db.sequelize

--- sever.js
+++ sever.js
@@ -9,12 +9,14 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+let db = require("./models")
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory to be served
-app.use(express.static("app/public"));
+app.use(express.static("/public"));
 
 // Routes
 // =============================================================
@@ -25,6 +27,6 @@ require("./app/routes/html-routes.js")(app);
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+db.sequelize.sync({ force: true }).then(app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
-});
+}));
