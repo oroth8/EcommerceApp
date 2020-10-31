e1e520da7390040e769f0c4e75977ee92266839b
k1te <kmiller343@gmail.com>
2020-10-30 19:17:21 -0500

    generated model via sequelize-cli

--- models/product.js
+++ models/product.js
@@ -1,11 +1,28 @@
-module.exports = function(sequelize, DataTypes) {
-    let Product = sequelize.define("Product", {
-        brand: DataTypes.STRING,
-        name: DataTypes.STRING,
-        category: DataTypes.STRING,
-        subCategory: DataTypes.STRING,
-        price: DataTypes.DECIMAL(10,2),
-        image_URLs: DataTypes.STRING
-    });
-    return Product;
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Product.init({
+    brand: DataTypes.STRING,
+    name: DataTypes.STRING,
+    category: DataTypes.STRING,
+    subCategory: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    image_URLs: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
 };
