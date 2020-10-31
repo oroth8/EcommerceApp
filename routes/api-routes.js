b7da2fb1cd6bc0564a9a97d69cdc42df1b286677
Justin Albert <jalbert@carthage.edu>
2020-10-30 19:03:15 -0500

    added .get to server for admin page search

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,4 +8,19 @@ module.exports = function(app) {
     res.render("index", {data:"Hello World!"})
   });
 
+
+  app.get("/admin/:table/:category?", function(req,res){
+    let table=req.params.table;
+    if(req.params.category){
+      let category=req.params.category;
+      db.table.findAll({
+        attributes: category,
+        })
+    }else{
+      db.table.findAll().then(function(results){
+        res.json(results);
+      })
+    }
+  }
+
 };
