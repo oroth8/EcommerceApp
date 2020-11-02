4a1a0ca95ad25b692260de2705261b569b56619e
Benjamin Dionysus <benjamin@sixbynine.com>
2020-11-02 15:16:31 -0600

    set default accessLEvel of new users to 10

--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     accessLevel:{
             type:INTEGER,
-            allowNull:false,
+            defaultValue:10
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
