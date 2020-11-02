50da750217cd046151347b1a6a4ac5939ddfac46
Benjamin Dionysus <benjamin@sixbynine.com>
2020-11-02 15:18:31 -0600

    added admin html requests and access level / login

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,30 +6,71 @@ const path=require("path");
 module.exports = function(app) {
 
     app.get("/", (req, res) => {
-        console.log("At the slash!");
-        console.log(req.user);
-        // If the user already has an account send them to the members page
-        if (req.user) {
-            res.render("landing",{layout:"main"});
-        }
-        else res.render("login");
-      });
+        let accessLevel=0;
+        if(req.user)accessLevel=req.user.accessLevel;
+        // People who have not logged in have an access level of 0
+        // Default uses have an access level of 10
+        // If we want to, we can make the defaul accesslevel lower, and make a new
+        // class of users, admins, with an access level of ten.
+        if(accessLevel>=10) accessGranted=true; else accessGranted=false;
+        // If we pass accessGranted=true to the main page, it will display the admin button
+        res.render("landing",{layout:"main", accessGranted:accessGranted});
+    });
       
     app.get("/login", (req, res) => {
         // If the user already has an account send them to the members page
         if (req.user) {
-            console.log("------------ Logged in");
-          res.render("landing",{layout:"main"});
+            if(req.user.accessLevel>=10) accessGranted=true; else accessGranted=false;
+            // If we pass accessGranted=true to the main page, it will display the admin button
+            res.render("landing",{layout:"main", accessGranted:accessGranted});
         }
         else res.render("login", {});
-      });
+      });      
+      app.get("/signup", (req, res) => {
+          // If the user already has an account send them to the members page
+          if (req.user) {
+            if(req.user.accessLevel>=10) accessGranted=true; else accessGranted=false;
+            // If we pass accessGranted=true to the main page, it will display the admin button
+            res.render("landing",{layout:"main", accessGranted:accessGranted});
+           
+          }
+          else  res.render("signup",{});
+        });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
+// ------------------------------------------
+// ----              ADMIN               ----
+// ------------------------------------------
+// --     Admin code                       --
+// --       (Most of this was in api-routes--
+// ------------------------------------------
 
+// Send the user to the admin home page.
+  app.get("/admin", function(req,res){
+    if (req.user) {
+        res.render("adminhome",{});
+      }
+      else res.render("login", {});
+    });   
+  
 
+    // Sends the user to adminadd page where they can add a product to the database.
+    app.get("/admin/:table/add", function(req,res){
+        if(req.user){
+            let table=req.params.table;
+            db[`${table}`].findAll({
+                where: {
+                id: 1
+            }
+            }).then(function(results){
+                let item=(results[0].dataValues);
+                res.render("adminadd", { "item": item});
+            });
+        }else res.render("login", {});
+    }); 
 
 };
