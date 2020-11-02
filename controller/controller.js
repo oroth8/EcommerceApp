6309a74126ec2567f0359738e6c3a70cf7ae843c
Benjamin Dionysus <benjamin@sixbynine.com>
2020-11-02 12:55:34 -0600

    trying to merge

--- controller/controller.js
+++ controller/controller.js
@@ -11,3 +11,17 @@ module.exports = {
         });  
     }
 }
+
+const db = require("../models");
+
+module.exports = {
+    allProducts: function(req, res) {
+        db.Product.findAll({}).then(function(dbProductData) {
+            let products = [];
+            for (let index = 0; index < dbProductData.length; index++) {
+                products.push(dbProductData[index].dataValues);
+            }
+            return products;
+        });  
+    }
+}
