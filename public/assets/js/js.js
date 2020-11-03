0b446938fd35b48d17d3ef23b0e79e5b800f470c
Justin Albert <jalbert@carthage.edu>
2020-11-03 17:03:39 -0600

    cart working

--- public/assets/js/js.js
+++ public/assets/js/js.js
@@ -8,14 +8,26 @@ if(limitCart===null){
 function addToCart(id){
     limitCart.push(id);
     localStorage.setItem("limitCart", JSON.stringify(limitCart)); 
+    updateCartCount();
 }
 
 function removeFromCart(id){
     limitCart.splice(limitCart.indexOf(id),1);
     localStorage.setItem("limitCart", JSON.stringify(limitCart)); 
+    updateCartCount();
 }
 
 
+$(".add-to-cart").on("click",function(e){
+    e.preventDefault();
+    addToCart(this.dataset.id);
+})
 
 // $("#reload-cart").on("click",viewCart);
-// $("#view-cart").on("click",viewCart);
+$("#cart-view").on("click",viewCart);
+
+function updateCartCount(){
+    $("#cart-count").text(limitCart.length);
+}
+
+updateCartCount();
