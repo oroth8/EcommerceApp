baca361ec40fa9537b16dbfc08a15b8861b1a8a5
Justin Albert <jalbert@carthage.edu>
2020-11-03 13:31:15 -0600

    Got Cart Working

--- public/assets/js/cart.js
+++ public/assets/js/cart.js
@@ -2,7 +2,11 @@ function viewCart(){
     let cart=JSON.parse(localStorage.getItem("limitCart"));
     $.post("/cart", {"limitCart": cart}).done(function(results){
         $("#cart-body").empty();
+        let newh=$("<h5>");
+        newh.text("Your Cart");
+        $("#cart-body").append(newh);
         let newUl=$("<ul>");
+        if(results[0].name){
         for(let i=0; i< results.length; i++){
             let newLi=$("<li>");
             newLi.html(`<img src="${results[i].img_URLs}" class="cart-img"> Name: ${results[i].name} Price: ${results[i].price} <button type="button" class="close" data-id="${results[i].id}" aria-label="Close">
@@ -17,7 +21,13 @@ function viewCart(){
             removeFromCart(id);
             viewCart();
         });
+        let totalPrice=0;
+        for(let i=0; i< results.length; i++){
+            totalPrice += Number(results[i].price);
+        }
+        $("#total-price").html("Total Price : " + totalPrice);
 
+        }
     });
 };
 
